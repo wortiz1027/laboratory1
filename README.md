d4612f9d555c1c7334dd39f23ea44616ffb5ffa8
Wilman Ortiz Navarro <willman.ortiz@gmail.com>
2023-03-20 14:40:02 -0500

    feat(scaffolding): Create base scaff for project

---
+++ README.md
@@ -0,0 +1,5 @@
+# LABORATORY 1
+
+## Reference Documentation
+
+[![pre-commit](https://img.shields.io/badge/pre--commit-enabled-brightgreen?logo=pre-commit)](https://github.com/pre-commit/pre-commit)
---
+++ src/test/java/dev/laboratory1/laboratory1/ApplicationTests.java
@@ -0,0 +1,11 @@
+package dev.laboratory1.laboratory1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
