0c5ae9abbce24cc7de1e046d5706346910f9d631
Wilman Ortiz Navarro <willman.ortiz@gmail.com>
2023-03-20 16:41:32 -0500

    feat(confguration): Add jacoco configuration

--- src/main/java/dev/laboratory/laboratory1/Application.java
+++ src/main/java/dev/laboratory/laboratory1/Application.java
@@ -1,4 +1,4 @@
-package dev.laboratory1.laboratory1;
+package dev.laboratory.laboratory1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
